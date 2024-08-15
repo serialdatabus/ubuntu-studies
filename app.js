20cc70315f20b32fad4024be509121b2b6395213
Elizeu <elizeucarlosc@gmail.com>
2024-08-15 12:41:40 +0000

    commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer(function(req, res){
+
+res.write('On the way to be a fulll stack engineer');
+res.end();
+
+}
+).listen(3000);
+console.log("Server started on port 3000");
